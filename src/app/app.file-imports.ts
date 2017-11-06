c2398fab2f25654aedec3049b85b4d432d5f5ec6
robinsoepboer <robinsoepboer@hotmail.com>
2017-11-07 00:30:08 +0100

    Removed react-contexify

--- src/app/app.file-imports.ts
+++ src/app/app.file-imports.ts
@@ -2,7 +2,6 @@
 // over to the build folder and thus these files will be taken along during distribution
 
 import '../styles/main.less';
-import 'file-loader?name=reactcontexify.css!../../node_modules/react-contexify/dist/ReactContexify.css';
 import 'file-loader?name=sweetalert.css!../../node_modules/sweetalert/dist/sweetalert.css';
 
 import 'file-loader?name=index.html!../index.html';
